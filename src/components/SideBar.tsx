fefd2a8418dd8049457164446fa80bbb6c931ee7
yue.lab <gitlab@rainy.me>
2019-02-13 23:54:37 +0900

    setup skeleton

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,47 @@
+import styled from "@emotion/styled";
+import React from "react";
+import { Link } from "react-router-dom";
+import routes from "../Routes";
+import { colors } from "../Theme";
+
+const GlobalSideBar = styled.aside`
+  width: 20%;
+  height: 100%;
+  background-color: #efefef;
+  border-right: 3px solid ${colors.bisque};
+`;
+
+const NavigatorList = styled.ul`
+  color: #000;
+  text-align: left;
+`;
+
+const NavigatorItem = styled.li`
+  transition: 0.3s all ease-in-out;
+  &:hover {
+    background-color: ${colors.lightseagreen};
+  }
+`;
+
+const NavigatorText = styled.span`
+  padding: 5px 15px;
+  display: block;
+  color: #000;
+  font-size: 24px;
+`;
+
+export default function SideBar() {
+  return (
+    <GlobalSideBar>
+      <NavigatorList>
+        {routes.map((route, key) => (
+          <NavigatorItem key={`sidebar-${key}`}>
+            <Link to={route.url}>
+              <NavigatorText>{route.title}</NavigatorText>
+            </Link>
+          </NavigatorItem>
+        ))}
+      </NavigatorList>
+    </GlobalSideBar>
+  );
+}
